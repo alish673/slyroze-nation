67eadc29714a75f1718200f61197792aac671e12
alish673 <alishahbazisydney@gmail.com>
2025-05-14 15:55:07 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neonPurple: '#C084FC',
+        neonGreen: '#32E875',
+        darkGray: '#1E1E1E',
+        slyrozePink: '#FF4FCC',
+        slyrozeBlue: '#5CFFFF'
+      },
+      boxShadow: {
+        neon: '0 0 10px rgba(192, 132, 252, 0.7), 0 0 20px rgba(192, 132, 252, 0.5)'
+      },
+      scale: {
+        102: '1.02',
+        105: '1.05',
+      },
+    },
   },
   plugins: [],
-}
+};
