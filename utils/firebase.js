4fc4dd18fe3eb66e46b34a4f46ecff49f771653f
alish673 <alishahbazisydney@gmail.com>
2025-05-15 06:24:45 +1000

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,7 +1,10 @@
+// Firebase Initialization for Slyroze Nation
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// Slyroze Nation Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCBmezwZVXcKCVcZyJxtV8Ub-SSO-70xYM",
   authDomain: "slyroze-nation.firebaseapp.com",
@@ -11,7 +14,9 @@ const firebaseConfig = {
   appId: "1:682230405880:web:7aa3a9495b153d4fa846a1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Export Firestore Database & Authentication Service
 export const db = getFirestore(app);
 export const auth = getAuth(app);
