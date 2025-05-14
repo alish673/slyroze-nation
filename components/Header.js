1e83cffcf8a37557d9af7ef9e6a75da67fe7c7d4
alish673 <alishahbazisydney@gmail.com>
2025-05-14 14:53:00 +1000

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-800 bg-opacity-50 backdrop-blur-md p-4 text-center shadow-md">
+      <img src="/assets/slyroze-logo.png" alt="Slyroze Logo" className="mx-auto w-32 mb-2 drop-shadow-lg" />
+      <h1 className="text-2xl font-semibold">Slyroze Nation Portal</h1>
+    </header>
+  );
+}
