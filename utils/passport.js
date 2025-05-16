3a2048ba9972704e090a6a1d285e22630a52dce4
alish673 <alishahbazisydney@gmail.com>
2025-05-16 14:20:58 +1000

    Update passport.js

--- utils/passport.js
+++ utils/passport.js
@@ -1,46 +1,37 @@
 import { ethers } from "ethers";
 
-// Updated addresses
+// Correct contract addresses
 const PASSPORT_CONTRACT_ADDRESS = "0xfd3BC111A9f4A0d86875f6B4Ca4Df592179CE0ca";
 const SLYP_CONTRACT_ADDRESS = "0x8E750e6E68f1378fEe36fEb74d8d28818b3B37b7";
 
-// ABI fragments
+// ABIs
 const SLYP_ABI = [
   "function allowance(address owner, address spender) view returns (uint256)",
-  "function approve(address spender, uint256 amount) returns (bool)",
-  "function transferFrom(address sender, address recipient, uint256 amount) returns (bool)",
-  "function burn(uint256 amount) returns (bool)"
+  "function approve(address spender, uint256 amount) returns (bool)"
 ];
 
 const PASSPORT_ABI = [
-  "function mintPassport(address to) returns (uint256)"
+  "function currentPrice() view returns (uint256)",
+  "function mintPassport()"
 ];
 
 export async function mintPassport(signer, userAddress) {
   const slyp = new ethers.Contract(SLYP_CONTRACT_ADDRESS, SLYP_ABI, signer);
   const passport = new ethers.Contract(PASSPORT_CONTRACT_ADDRESS, PASSPORT_ABI, signer);
 
-  const mintCost = ethers.utils.parseUnits("200", 18);
-  const burnAmount = ethers.utils.parseUnits("2", 18);
+  // Get dynamic mint price from contract
+  const price = await passport.currentPrice();
 
-  // Step 1: Approve if needed
+  // Check SLYP allowance
   const allowance = await slyp.allowance(userAddress, PASSPORT_CONTRACT_ADDRESS);
-  if (allowance.lt(mintCost)) {
-    const approveTx = await slyp.approve(PASSPORT_CONTRACT_ADDRESS, mintCost);
+  if (allowance.lt(price)) {
+    const approveTx = await slyp.approve(PASSPORT_CONTRACT_ADDRESS, price);
     await approveTx.wait();
   }
 
-  // Step 2: Transfer SLYP to contract
-  const transferTx = await slyp.transferFrom(userAddress, PASSPORT_CONTRACT_ADDRESS, mintCost);
-  await transferTx.wait();
-
-  // Step 3: Mint the Passport to user
-  const mintTx = await passport.mintPassport(userAddress);
+  // Mint passport
+  const mintTx = await passport.mintPassport();
   await mintTx.wait();
-
-  // Step 4: Burn 2 SLYP
-  const burnTx = await slyp.burn(burnAmount);
-  await burnTx.wait();
 
   return true;
 }
