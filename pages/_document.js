7d41b14b3798e2a996766b35819074293731dad7
alish673 <alishahbazisydney@gmail.com>
2025-05-17 11:03:41 +1000

    Delete pages/_document.js

--- pages/_document.js
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head';
-
-<Head>
-  <title>Slyroze Nation</title>
-  <link rel="icon" href="/favicon.ico" />
-  <link rel="apple-touch-icon" href="/logo.png" />
-  <meta name="theme-color" content="#000000" />
-  <meta name="description" content="Slyroze (SLY) is a BNB-powered token and digital nation ecosystem." />
-</Head>
