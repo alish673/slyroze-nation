bc6e56ec6a26c63ca996228ae897c32b4fbcefdf
alish673 <alishahbazisydney@gmail.com>
2025-05-18 09:21:35 +1000

    Update leaderboard.js

--- utils/leaderboard.js
+++ utils/leaderboard.js
@@ -2,16 +2,20 @@ import { db } from "./firebase";
 import { collection, query, orderBy, limit, getDocs } from "firebase/firestore";
 
 export async function getLeaderboard() {
-  const q = query(collection(db, "users"), orderBy("slyp", "desc"), limit(10));
+  const q = query(collection(db, "users"), orderBy("claimedZones", "desc"), limit(10));
   const snapshot = await getDocs(q);
   const leaders = [];
+
   snapshot.forEach(doc => {
     const data = doc.data();
     leaders.push({
-      alias: data.alias || data.email,
+      alias: data.alias || data.username || "Unnamed",
+      claimedZones: data.claimedZones || 0,
+      hasPassport: data.hasPassport || false,
       slyp: data.slyp || 0,
       lands: data.lands || []
     });
   });
+
   return leaders;
 }
